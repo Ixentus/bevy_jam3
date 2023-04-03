33b507e6a75621c1aa38a3bc38d182c4b24c537b
Acid <alexvermaat@gmail.com>
2023-04-03 14:09:12 +0200

    Splash screen state

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,19 @@
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
+use std::time::Duration;
+
 use bevy::prelude::*;
 use bevy::window::{PresentMode, WindowMode, WindowPlugin};
 use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
+
+#[derive(Clone, PartialEq, Eq, Default, Debug, Hash, States)]
+pub enum AppState {
+    #[default]
+    Splash,
+    InGame,
+    GameOver,
+}
 
 fn main() {
     App::new()
@@ -23,7 +33,9 @@ fn main() {
         .insert_resource(FramepaceSettings {
             limiter: Limiter::Auto,
         })
+        .add_state::<AppState>()
         .add_startup_system(setup)
+        .add_system(splash.in_set(OnUpdate(AppState::Splash)))
         .run();
 }
 
@@ -33,4 +45,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("icon.png"),
         ..Default::default()
     });
+}
+
+fn splash(mut commands: Commands, mut state: ResMut<NextState<AppState>>, time: Res<Time>, icon: Query<Entity, With<Sprite>>) {
+    if time.startup().elapsed() > Duration::new(2, 0) {
+        state.set(AppState::InGame);
+        commands.entity(icon.single()).despawn();
+    }
 }
